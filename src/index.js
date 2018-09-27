1c15abc1ff1c23e27cd6bef4f8c270b6d3e236e9
mikhailnakanechny <mikhailnakanechny@gmail.com>
2018-09-27 22:31:06 +0300

    20 ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let state=false;
+    let arr = str.split("");
+    let arrCheck = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (arr[i] === bracketsConfig[j][1] && arrCheck[arrCheck.length - 1] === bracketsConfig[j][0]) {
+                arrCheck.pop();
+                break;
+            } else if (arr[i] === bracketsConfig[j][0]) {
+                arrCheck.push(arr[i]);
+                break;
+            } else if (arr[i] === bracketsConfig[j][1]
+                && (arrCheck.length === 0 || arrCheck[arrCheck.length - 1] !== bracketsConfig[j][0])) {
+                return false;
+            }
+        }
+    }
+    state = arrCheck.length === 0 ? true : false;
+    return state;
+    //  console.log(state);
+};
+
